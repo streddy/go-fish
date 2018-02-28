e8be333c33464c28a07bd1dba096bfffda4a7bc6
Sanjeev Reddy <sanjeev96red@gmail.com>
2018-02-28 04:22:32 -0800

    handle all of traffic generation. untested

--- gofish.go
+++ gofish.go
@@ -7,13 +7,74 @@ import (
     "net/http"
     "fmt"
     "strings"
+    "sync"
     "time"
 )
 
-func PrepareBait(requestInfo structs.TackleBox) *structs.Bait {
+// Hits routes and their dependencies in a random order under specified traffic conditions
+func GoFish(trafficParams structs.TackleBox, responseChannels []chan *structs.Response,
+            doneChannel chan bool) {
+    random := rand.New(rand.NewSource(time.Now().UnixNano()))
+
+    // Send a request every 1/Frequency seconds (at fastest)
+    ticker := time.NewTicker(time.Second / time.Duration(trafficParams.Frequency))
+    for range ticker.C {
+        // if caller has decided to stop sending traffic, break
+        if len(doneChannel) == 1 {
+            break
+        }
+
+        // pick a random route
+        index := random.Intn(len(trafficParams.Routes))
+        route := trafficParams.Routes[index]
+
+        // construct BaitBox struct for this route
+        requestInfo := structs.BaitBox{
+            Route:      route,
+            Transport:  trafficParams.Transport,
+            MinLatency: trafficParams.MinLatency,
+            MaxLatency: trafficParams.MaxLatency,
+        }
+
+        // prepare request-specific fields
+        request := prepareBait(requestInfo)
+
+        // send request
+        requestStart := time.Now()
+        response := castReel(request)
+
+        // hit all of the route's dependencies
+        for _, dependency := range route.MandatoryDependencies {
+            requestInfo.Route = dependency
+			request := prepareBait(requestInfo)
+            castReel(request)
+        }
+        
+        // fill remaining response fields and place response in channel
+        response.Start = requestStart
+        response.Duration = time.Since(requestStart).Nanoseconds()
+        responseChannels[index] <- response
+    }
+}
+
+// HELPER FUNCTIONS
+
+// Prepares a Bait struct that will be used in sending the request
+func prepareBait(requestInfo structs.BaitBox) *structs.Bait {
     // construct request
-    request, _ := http.NewRequest(requestInfo.Method, requestInfo.Url,
-                                  requestInfo.RequestBodyReader)
+    route = requestInfo.Route
+    requestBodyReader := strings.NewReader(route.RequestBody)
+    request, _ := http.NewRequest(route.Method, route.Url, requestBodyReader)
+
+    // split incoming header string by \n and build header pairs
+    headerPairs := strings.Split(route.Headers, "\n")
+    for i := range headerPairs {
+        split := strings.SplitN(headerPairs[i], ":", 2)
+		if len(split) == 2 {
+			request.Header.Set(split[0], split[1])
+		}
+    }
+    request.Header.Set("go_time", time.Now().String())
     
     // determine a random latency for this request
     random := rand.New(rand.NewSource(time.Now().UnixNano()))
@@ -32,8 +93,36 @@ func PrepareBait(requestInfo structs.TackleBox) *structs.Bait {
     return bait
 }
 
-func GoFish(request *structs.Bait) *http.Response {
+// Sends a request and introduces traffic conditions
+func castReel(request *structs.Bait) *structs.Response {
+    // introduce latency
     time.sleep(*request.Latency)
+
+    // send request and handle response
     httpResponse, err := *request.Transport.RoundTrip(*request.Request)
-    return httpResponse
+    response := reelIn(httpResponse, err != nil)
+
+    return response
+}
+
+// Parses the response and returns it to caller
+func reelIn(httpResponse *http.Response, err bool) *structs.Response {
+    response := &structs.Response{}
+    
+	if err {
+		response.Error = true
+	} else {
+		if httpResponse.ContentLength < 0 { // -1 if the length is unknown
+			content, err := ioutil.ReadAll(httpResponse.Body)
+			if err == nil {
+				response.Size = int64(len(content))
+			}
+		} else {
+			response.Size = httpResponse.ContentLength
+		}
+		response.StatusCode = httpResponse.StatusCode
+		defer httpResponse.Body.Close()
+	}
+
+	return response
 }
