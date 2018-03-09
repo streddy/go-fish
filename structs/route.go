31177bcf564027134195fbb1892d52ad0f9d3f89
Sanjeev Reddy <sanjeev96red@gmail.com>
2018-03-09 03:36:05 -0800

    another tab fix

--- structs/route.go
+++ structs/route.go
@@ -2,11 +2,11 @@ package structs
 
 // Route is a struct that contains information about a given route to be hit
 type Route struct {
-    Url			    string
-    Method		    string
-    Headers		    string
-    RequestBody		    string
+    Url                     string
+    Method                  string
+    Headers                 string
+    RequestBody             string
     MandatoryDependencies   []Route
     LikelyDependencies	    []Route
-    Samples		    int
+    Samples                 int
 }
