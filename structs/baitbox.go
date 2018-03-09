845314f5d47300f658f5399bbda85ca4c410ccb3
Sanjeev Reddy <sanjeev96red@gmail.com>
2018-03-09 03:34:27 -0800

    tab fix again

--- structs/baitbox.go
+++ structs/baitbox.go
@@ -6,7 +6,7 @@ import (
 
 // BaitBox is a struct used as an intermediary in creating a request (Bait struct)
 type BaitBox struct {
-    Route 	Route
+    Route       Route
     Transport 	*http.Transport
     MinLatency	string
     MaxLatency 	string
